2905c7b1339ffb8369347125d667f08f5c6a775f
maxkorzan <maxkorzan@gmail.com>
2020-01-23 09:43:44 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x;
+//
+//        //prompt user for int
+//        System.out.println("Enter a number: ");
+//        x  = sc.nextInt();
+//        //SCANNER BUFFER - NEED THIS TO "EAT" UP THE USER'S LAST "ENTER" INPUT
+//        sc.nextLine();
+//        //prompt user for String
+//        System.out.println("Enter a string: ");
+//        String y = sc.nextLine();
+
+
+        //READING AN EMPTY LINE
+        //(example code missing)
+
+
+        //RANDOM
+        Random rand = new Random();
+        int x = 1+ rand.nextInt(6);
+
+        //STRING METHODS
+
+
+
+    }
+}
