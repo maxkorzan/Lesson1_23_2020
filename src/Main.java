5b876910462934211379f7c16251fe7cbc64cc1e
maxkorzan <maxkorzan@gmail.com>
2020-01-23 10:26:36 -0500

    added missing examples

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-//        int x;
-//
-//       //prompt user for int
-//        System.out.println("Enter a number: ");
-//        x  = sc.nextInt();
-//        //SCANNER BUFFER - NEED THIS TO "EAT" UP THE USER'S LAST "ENTER" INPUT
-//        sc.nextLine();
-//        //prompt user for String
-//        System.out.println("Enter a string: ");
-//        String y = sc.nextLine();
+        int x;
+
+       //prompt user for int
+        System.out.println("Enter a number: ");
+        x  = sc.nextInt();
+        //SCANNER BUFFER - NEED THIS TO "EAT" UP THE USER'S LAST "ENTER" INPUT
+        sc.nextLine();
+        //prompt user for String
+        System.out.println("Enter a string: ");
+        String word = sc.nextLine();
 
 
         //READING AN EMPTY LINE
-        //(example code missing)
+        String answer = "y";
+        while (answer.equalsIgnoreCase("y")){
+            System.out.println("Please enter something: ");
+            String something = sc.nextLine();
+            if (something.equals("")){
+                break;
+            }
+            System.out.println(something);
+        }
 
 
         //RANDOM
         Random rand = new Random();
-        int x = 1+ rand.nextInt(6);
+        int y = 1+ rand.nextInt(6);
         System.out.println(x);
 
         //STRING METHODS
@@ -33,9 +41,6 @@ public class Main {
         System.out.println(word2.length());
         //charat
         System.out.println(word2.charAt(3));
-
-
-
 
     }
 }
